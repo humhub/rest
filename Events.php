b06a8cbdbf01e89db3c21f090a9740e8f5328b5f
yurabakhtin <yurybakh@gmail.com>
2020-10-29 13:40:31 +0300

    Current user endpoint

--- Events.php
+++ Events.php
@@ -34,6 +34,7 @@ class Events
             ['pattern' => 'api/v1/user/', 'route' => 'rest/user/user/index', 'verb' => ['GET', 'HEAD']],
             ['pattern' => 'api/v1/user/get-by-username', 'route' => 'rest/user/user/get-by-username', 'verb' => ['GET', 'HEAD']],
             ['pattern' => 'api/v1/user/get-by-email', 'route' => 'rest/user/user/get-by-email', 'verb' => ['GET', 'HEAD']],
+            ['pattern' => 'api/v1/user/current', 'route' => 'rest/user/user/current', 'verb' => ['GET', 'HEAD']],
             ['pattern' => 'api/v1/user/<id:\d+>', 'route' => 'rest/user/user/view', 'verb' => ['GET', 'HEAD']],
             ['pattern' => 'api/v1/user/<id:\d+>', 'route' => 'rest/user/user/update', 'verb' => ['PUT', 'PATCH']],
             ['pattern' => 'api/v1/user/<id:\d+>', 'route' => 'rest/user/user/delete', 'verb' => ['DELETE']],
