eba89103f4e9087a4b9d4aac5c24f8fb674a4fb1
yurabakhtin <yurybakh@gmail.com>
2021-02-26 16:25:27 +0300

    Url prefix for API endpoints

--- Module.php
+++ Module.php
@@ -17,7 +17,15 @@ use yii\web\JsonParser;
 class Module extends \humhub\components\Module
 {
 
+    /**
+     * @event triggered before a request (inside event Application::EVENT_BEFORE_REQUEST of this Module)
+     */
     const EVENT_REST_API_ADD_RULES = 'restApiAddRules';
+
+    /**
+     * @var string Prefix for REST API endpoint URLs
+     */
+    const API_URL_PREFIX = 'api/v1/';
 
     /**
      * @inheritdoc
@@ -44,6 +52,22 @@ class Module extends \humhub\components\Module
     public function getConfigUrl()
     {
         return Url::to(['/rest/admin/index']);
+    }
+
+    /**
+     * Add REST API endpoint rules
+     *
+     * @param array $rules
+     */
+    public function addRules($rules)
+    {
+        foreach ($rules as $r => $rule) {
+            if (isset($rule['pattern'])) {
+                $rules[$r]['pattern'] = self::API_URL_PREFIX . ltrim($rule['pattern'], '/');
+            }
+        }
+
+        Yii::$app->urlManager->addRules($rules);
     }
 
 }
