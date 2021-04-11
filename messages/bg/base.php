335046a14d69b92efcfc80117a11ec79c9f6f901
HumHub Translations <info@humhub.com>
2021-04-11 14:34:44 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/bg/base.php
+++ messages/bg/base.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => '',
-    'Enabled for all registered users' => '',
-    'JWT Key' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => '<strong>API</strong> Конфигурация',
+  'Enabled for all registered users' => 'Активиран за всички регистрирани потребители',
+  'JWT Key' => 'Ключ JWT',
+);
