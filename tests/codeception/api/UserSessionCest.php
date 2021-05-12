3a29b7c06ad01ada1628bf5c07ab763bf12027de
yurabakhtin <yurybakh@gmail.com>
2021-05-12 17:12:48 +0300

    API tests: User - Session Controller

---
+++ tests/codeception/api/UserSessionCest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace rest\api;
+
+use rest\ApiTester;
+use tests\codeception\_support\HumHubApiTestCest;
+
+class UserSessionCest extends HumHubApiTestCest
+{
+    public function testList(ApiTester $I)
+    {
+        $I->wantTo('delete all sessions by user id');
+        $I->amAdmin();
+
+        $I->sendDelete('user/session/all/1');
+        $I->seeSuccessMessage('1 user sessions deleted!');
+
+        $I->sendDelete('user/session/all/2');
+        $I->seeSuccessMessage('0 user sessions deleted!');
+
+        $I->sendDelete('user/session/all/123');
+        $I->seeNotFoundMessage('User not found!');
+    }
+
+}
