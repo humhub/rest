6fe7c2d6c518cf8ce2eccb2ed6b767e1f965d4fb
HumHub Translations <info@humhub.com>
2021-06-10 20:14:33 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => '',
-    'Active additional REST API endpoints from the modules' => '',
-    'Allow HTTP Basic Authentication' => '',
-    'Enabled for all registered users' => '',
-    'JWT Key' => '',
-    'JWT Token Expiration' => '',
-    'No enabled modules found with additional REST API endpoints.' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => '<strong>API</strong> การกำหนดค่า',
+  'Active additional REST API endpoints from the modules' => 'ใช้งานปลายทาง REST API เพิ่มเติมจากโมดูล',
+  'Allow HTTP Basic Authentication' => 'อนุญาตการตรวจสอบสิทธิ์พื้นฐานของ HTTP',
+  'Enabled for all registered users' => 'เปิดใช้งานสำหรับผู้ใช้ที่ลงทะเบียนทั้งหมด',
+  'JWT Key' => 'JWT คีย์',
+  'JWT Token Expiration' => 'JWT Token หมดอายุ',
+  'No enabled modules found with additional REST API endpoints.' => 'ไม่พบโมดูลที่เปิดใช้งานพร้อมปลายทาง REST API เพิ่มเติม',
+);
