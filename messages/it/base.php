97bea39a9f46a9d552634d9e6343ce83dfcf0e81
HumHub Translations <info@humhub.com>
2021-07-17 21:50:15 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/base.php
+++ messages/it/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => 'Configurazione <strong>API</strong>',
-    'Enabled for all registered users' => 'Abilitato per tutti gli utenti registrati',
-    'JWT Key' => 'Key JWT',
-    'Active additional REST API endpoints from the modules' => '',
-    'Allow HTTP Basic Authentication' => '',
-    'JWT Token Expiration' => '',
-    'No enabled modules found with additional REST API endpoints.' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => 'Configurazione <strong>API</strong>',
+  'Active additional REST API endpoints from the modules' => 'Endpoint API REST aggiuntivi attivi dai moduli',
+  'Allow HTTP Basic Authentication' => 'Consenti autenticazione di base HTTP',
+  'Enabled for all registered users' => 'Abilitato per tutti gli utenti registrati',
+  'JWT Key' => 'Key JWT',
+  'JWT Token Expiration' => 'Scadenza token JWT',
+  'No enabled modules found with additional REST API endpoints.' => 'Nessun modulo abilitato trovato con endpoint API REST aggiuntivi.',
+);
