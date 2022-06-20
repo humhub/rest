c14211994b2a6ba0cec33dd0997bd83dae7f56db
HumHub Translations <hello@humhub.com>
2022-06-20 16:41:21 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/cy/base.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '<strong>API</strong> Configuration' => '',
+    'Active additional REST API endpoints from the modules' => '',
+    'Allow HTTP Basic Authentication' => '',
+    'Enabled for all registered users' => '',
+    'JWT Key' => '',
+    'JWT Token Expiration' => '',
+    'No enabled modules found with additional REST API endpoints.' => '',
+];
