77a175861f474bdd3aaf88218c6f210d0f9b4fc5
HumHub Translations <hello@humhub.com>
2023-01-08 15:46:37 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => '<strong>API</strong> beállítás',
-    'Enabled for all registered users' => 'Engedélyezés az összes felhasználónak',
-    'JWT Key' => 'JWT kulcs',
-    'Active additional REST API endpoints from the modules' => '',
-    'Allow HTTP Basic Authentication' => '',
-    'JWT Token Expiration' => '',
-    'No enabled modules found with additional REST API endpoints.' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => '<strong>API</strong> beállítás',
+  'Active additional REST API endpoints from the modules' => 'Aktív további REST API-végpontok a modulokból',
+  'Allow HTTP Basic Authentication' => 'HTTP alapszintű hitelesítés engedélyezése',
+  'Enabled for all registered users' => 'Engedélyezés az összes felhasználónak',
+  'JWT Key' => 'JWT kulcs',
+  'JWT Token Expiration' => 'JWT token lejárata',
+  'No enabled modules found with additional REST API endpoints.' => 'Nem találhatók engedélyezett modulok további REST API-végpontokkal.',
+);
--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => '',
-    'Active additional REST API endpoints from the modules' => '',
-    'Allow HTTP Basic Authentication' => '',
-    'Enabled for all registered users' => '',
-    'JWT Key' => '',
-    'JWT Token Expiration' => '',
-    'No enabled modules found with additional REST API endpoints.' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => 'Configuração <strong>API</strong>',
+  'Active additional REST API endpoints from the modules' => '',
+  'Allow HTTP Basic Authentication' => '',
+  'Enabled for all registered users' => '',
+  'JWT Key' => '',
+  'JWT Token Expiration' => '',
+  'No enabled modules found with additional REST API endpoints.' => '',
+);
