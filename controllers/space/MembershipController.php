f4262bbdf24a2979828338b60e1f970c28729449
Gev <gevorgmansuryan@gmail.com>
2023-04-15 23:48:31 +0400

    Refactor Bearer Auth

--- controllers/space/MembershipController.php
+++ controllers/space/MembershipController.php
@@ -101,6 +101,14 @@ class MembershipController extends BaseController
             return $this->returnError(400, 'Invalid role given!');
         }
 
+        if ($showAtDashboard = Yii::$app->request->get('showAtDashboard')) {
+            $membership->show_at_dashboard = (int) $showAtDashboard;
+        }
+
+        if ($canLeave = Yii::$app->request->get('canLeave')) {
+            $membership->can_cancel_membership = !! $canLeave;
+        }
+
         $membership->group_id = $newRole;
         $membership->save();
 
