60522aa16fb3ddd8edbdbe63ec0856d5dc208546
HumHub Translations <hello@humhub.com>
2023-04-28 18:26:32 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '<strong>API</strong> Configuration' => '',
-    'Active additional REST API endpoints from the modules' => '',
-    'Allow HTTP Basic Authentication' => '',
-    'Enabled for all registered users' => '',
-    'JWT Key' => '',
-    'JWT Token Expiration' => '',
-    'No enabled modules found with additional REST API endpoints.' => '',
-];
+return array (
+  '<strong>API</strong> Configuration' => '<strong>API</strong> の構成',
+  'Active additional REST API endpoints from the modules' => 'モジュールからのアクティブな追加の REST API エンドポイント',
+  'Allow HTTP Basic Authentication' => 'HTTP 基本認証を許可する',
+  'Enabled for all registered users' => '全ての登録ユーザーに対して有効',
+  'JWT Key' => 'JWT キー',
+  'JWT Token Expiration' => 'JWT トークンの有効期限',
+  'No enabled modules found with additional REST API endpoints.' => '追加の REST API エンドポイントで有効なモジュールが見つかりません。',
+);
