9f4e65825e784bc3b0f773358ddc21256140acd9
Yuriy Bakhtin <yurybakh@gmail.com>
2026-01-16 17:19:39 +0300

    Fix test fixtures data

--- tests/codeception/fixtures/data/like.php
+++ tests/codeception/fixtures/data/like.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    ['object_model' => 'humhub\modules\post\models\Post', 'object_id' => 1, 'created_at' => '2021-05-13 09:33:00', 'created_by' => 3, 'updated_at' => '2021-05-13 09:33:00', 'updated_by' => 3],
-    ['object_model' => 'humhub\modules\post\models\Post', 'object_id' => 2, 'created_at' => '2021-05-13 09:34:00', 'created_by' => 2, 'updated_at' => '2021-05-13 09:34:00', 'updated_by' => 2],
-    ['object_model' => 'humhub\modules\post\models\Post', 'object_id' => 1, 'created_at' => '2021-05-13 09:36:00', 'created_by' => 4, 'updated_at' => '2021-05-13 09:36:00', 'updated_by' => 4],
+    ['content_id' => 1, 'created_at' => '2021-05-13 09:33:00', 'created_by' => 3, 'updated_at' => '2021-05-13 09:33:00', 'updated_by' => 3],
+    ['content_id' => 2, 'created_at' => '2021-05-13 09:34:00', 'created_by' => 2, 'updated_at' => '2021-05-13 09:34:00', 'updated_by' => 2],
+    ['content_id' => 1, 'created_at' => '2021-05-13 09:36:00', 'created_by' => 4, 'updated_at' => '2021-05-13 09:36:00', 'updated_by' => 4],
 ];
