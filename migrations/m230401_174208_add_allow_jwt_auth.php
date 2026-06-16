de484ee726c54b2153f0ee6567c55c2818b90432
GitHub Action <action@github.com>
2026-06-16 14:14:22 +0000

    Autocommit PHP CS Fixer

--- migrations/m230401_174208_add_allow_jwt_auth.php
+++ migrations/m230401_174208_add_allow_jwt_auth.php
@@ -9,16 +9,18 @@ class m230401_174208_add_allow_jwt_auth extends Migration
     {
         $jwtKey = $this->db->createCommand(
             'SELECT value FROM setting WHERE module_id = :m AND name = :n',
-            [':m' => 'rest', ':n' => 'jwtKey']
+            [':m' => 'rest', ':n' => 'jwtKey'],
         )->queryScalar();
 
-        $this->upsert('setting',
+        $this->upsert(
+            'setting',
             ['module_id' => 'rest', 'name' => 'enableJwtAuth', 'value' => !empty($jwtKey) ? '1' : '0'],
-            ['value' => !empty($jwtKey) ? '1' : '0']
+            ['value' => !empty($jwtKey) ? '1' : '0'],
         );
-        $this->upsert('setting',
+        $this->upsert(
+            'setting',
             ['module_id' => 'rest', 'name' => 'enableBasicAuth', 'value' => '1'],
-            ['value' => '1']
+            ['value' => '1'],
         );
     }
 
